857e54fdd7b2127c2d99bcd54920de1a97d76fe6
miricoder <mirali.mirzyev@hotmai.com>
2020-09-27 15:53:13 -0400

    Fix13

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -11,7 +11,7 @@ echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 #npm install --save-dev cross-env
-#sudo npm install --unsafe-perm=true --allow-root
+sudo npm install --unsafe-perm=true --allow-root
 set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
